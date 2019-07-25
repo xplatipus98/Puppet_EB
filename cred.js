be830ed40054a1564e7934696aae5f8ee650382e
sparsh <sparshsadafal14@gmail.com>
2019-07-25 19:58:19 +0530

    First Commit

---
+++ cred.js
@@ -0,0 +1,3 @@
+module.exports = {
+    username: 'riteshsengar26@gmail.com',
+    password: 'mindcrew01'}
---
+++ index.js
@@ -0,0 +1,30 @@
+const CREDS = require('./cred');
+
+const puppeteer = require('puppeteer');
+
+async function run() {
+  const browser = await puppeteer.launch({
+    headless: false
+  });
+  const page = await browser.newPage();
+  
+  await page.goto('https://github.com/login');
+  const USERNAME_SELECTOR = '#login_field';
+const PASSWORD_SELECTOR = '#password';
+const BUTTON_SELECTOR = '#login > form > div.auth-form-body.mt-3 > input.btn.btn-primary.btn-block';
+
+await page.click(USERNAME_SELECTOR);
+await page.keyboard.type(CREDS.username);
+
+await page.click(PASSWORD_SELECTOR);
+await page.keyboard.type(CREDS.password);
+
+await page.click(BUTTON_SELECTOR);
+
+await page.waitForNavigation();
+  
+  
+  //browser.close();
+}
+
+run();
